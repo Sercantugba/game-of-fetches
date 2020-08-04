572c4cdecdb924b494a0f2bf7b3aff39526ee598
Sercantugba <sercantugba834@gmail.com>
2020-08-04 10:02:14 -0500

    created class components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Game of Thrones </h1>
+      </div>
+    )
+  }
 }
 
 export default App;
